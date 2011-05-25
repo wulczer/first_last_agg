350539206f87dac80a01fdd51513e003e6208d8c
Jan Urbański <wulczer@wulczer.org>
2011-05-25 19:30:01 +0200

    Fix script to upgrade from unpackaged.

--- sql/first_last_agg--unpackaged--0.1.0.sql
+++ sql/first_last_agg--unpackaged--0.1.0.sql
@@ -1,4 +1,4 @@
-ALTER EXTENSION "first-last-agg" ADD FUNCTION first_sfunc(anyelement, anyelement);
-ALTER EXTENSION "first-last-agg" ADD FUNCTION last_sfunc(anyelement, anyelement);
-ALTER EXTENSION "first-last-agg" ADD AGGREGATE last(anyelement);
-ALTER EXTENSION "first-last-agg" ADD AGGREGATE first(anyelement);
+ALTER EXTENSION first_last_agg ADD FUNCTION first_sfunc(anyelement, anyelement);
+ALTER EXTENSION first_last_agg ADD FUNCTION last_sfunc(anyelement, anyelement);
+ALTER EXTENSION first_last_agg ADD AGGREGATE last(anyelement);
+ALTER EXTENSION first_last_agg ADD AGGREGATE first(anyelement);
