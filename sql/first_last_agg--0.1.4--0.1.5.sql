dd0f7876af312061ef70c440825f310e9b26fa99
Mael Rimbault <mael.rimbault@adjust.com>
2020-07-03 17:15:12 +0200

    Bump the version to 0.1.5
    Adds an update script to set aggregate functions to parallel-safe.

---
+++ sql/first_last_agg--0.1.4--0.1.5.sql
@@ -0,0 +1,30 @@
+/*
+ * Make aggregate functions parallel safe, starting with PG10.
+ */
+DO $$
+DECLARE version_num integer;
+BEGIN
+  SELECT current_setting('server_version_num') INTO STRICT version_num;
+  IF version_num > 90600 THEN
+    EXECUTE $E$  ALTER FUNCTION last_sfunc(anyelement, anyelement) PARALLEL SAFE   $E$;
+    EXECUTE $E$ ALTER FUNCTION first_sfunc(anyelement, anyelement) PARALLEL SAFE   $E$;
+
+    EXECUTE $E$ DROP AGGREGATE IF EXISTS first(anyelement) $E$;
+    EXECUTE $E$ CREATE AGGREGATE first(anyelement) (
+        SFUNC = first_sfunc,
+        STYPE = anyelement,
+        COMBINEFUNC = first_sfunc,
+        parallel = SAFE
+    ); $E$;
+
+    EXECUTE $E$ DROP AGGREGATE IF EXISTS last(anyelement) $E$;
+    EXECUTE $E$ CREATE AGGREGATE last(anyelement) (
+        SFUNC = last_sfunc,
+        STYPE = anyelement,
+        COMBINEFUNC = last_sfunc,
+        parallel = SAFE
+    ); $E$;
+  END IF;
+END;
+$$;
+
